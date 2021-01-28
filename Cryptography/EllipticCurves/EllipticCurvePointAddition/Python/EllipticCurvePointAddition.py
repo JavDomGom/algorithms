0b747a4b7b898a2bed40d9b79de592d8b3f1e8dd
JavDomGom <jdomgo3@gmail.com>
2021-01-28 12:46:08 +0100

    Add Elliptic curve point addition operation in Python.

--- Cryptography/EllipticCurves/EllipticCurvePointAddition/Python/EllipticCurvePointAddition.py
+++ Cryptography/EllipticCurves/EllipticCurvePointAddition/Python/EllipticCurvePointAddition.py
@@ -14,16 +14,19 @@ x_2 = 4
 y_2 = 1
 
 print('1. y_1-y_2 ; x_1-x_2')
+
 op_x = x_1-x_2
 op_y = y_1-y_2
 
 inv_op_x = invert(op_x, p)
+
 print(
     f'2. 1/(x_1 - x_2) mod p = \
 1/{op_x} mod {p} = {op_x}⁻¹ mod {p} = {inv_op_x}'
 )
 
 lam = op_y*inv_op_x % p
+
 print(
     f'3. λ = (y_1 - y_2)/(x_1 - x_2) mod p = \
 {op_y}/{op_x} mod {p} = \
@@ -32,12 +35,14 @@ print(
 )
 
 x = (pow(lam, 2)-x_1-x_2) % p
+
 print(
     f'4. x = λ² - x_1 - x_2 mod p = \
 {lam}²-{x_1}-{x_2} mod {p} = {pow(lam, 2)-x_1-x_2} mod {p} = {x}'
 )
 
 y = (lam*(x_1-x)-y_1) % p
+
 print(
     f'5. y = λ(x_1 - x) - y_1 mod p = \
 {lam}({x_1}-{x})-{y_1} mod {p} = \
