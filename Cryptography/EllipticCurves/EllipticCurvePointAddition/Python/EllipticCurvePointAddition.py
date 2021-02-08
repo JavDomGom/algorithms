8f918d6313fd89e21211e99ac0977c03a973b907
JavDomGom <jdomgo3@gmail.com>
2021-02-08 19:02:41 +0100

    Improve example in Python code.

--- Cryptography/EllipticCurves/EllipticCurvePointAddition/Python/EllipticCurvePointAddition.py
+++ Cryptography/EllipticCurves/EllipticCurvePointAddition/Python/EllipticCurvePointAddition.py
@@ -13,10 +13,10 @@ y_1 = 5
 x_2 = 4
 y_2 = 1
 
-print('1. y_1-y_2 ; x_1-x_2')
+print('1. y_1 - y_2 ; x_1 - x_2')
 
-op_x = x_1-x_2
-op_y = y_1-y_2
+op_x = x_1 - x_2
+op_y = y_1 - y_2
 
 inv_op_x = invert(op_x, p)
 
@@ -25,7 +25,7 @@ print(
 1/{op_x} mod {p} = {op_x}⁻¹ mod {p} = {inv_op_x}'
 )
 
-lam = op_y*inv_op_x % p
+lam = op_y * inv_op_x % p
 
 print(
     f'3. λ = (y_1 - y_2)/(x_1 - x_2) mod p = \
@@ -34,14 +34,14 @@ print(
 {op_y}*{inv_op_x} mod {p} = {lam}'
 )
 
-x = (pow(lam, 2)-x_1-x_2) % p
+x = (pow(lam, 2) - x_1 - x_2) % p
 
 print(
     f'4. x = λ² - x_1 - x_2 mod p = \
 {lam}²-{x_1}-{x_2} mod {p} = {pow(lam, 2)-x_1-x_2} mod {p} = {x}'
 )
 
-y = (lam*(x_1-x)-y_1) % p
+y = (lam * (x_1-x) - y_1) % p
 
 print(
     f'5. y = λ(x_1 - x) - y_1 mod p = \
