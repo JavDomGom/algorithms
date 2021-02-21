3a02ff41252e3c4f700b47398ba505cde63334f9
JavDomGom <jdomgo3@gmail.com>
2021-02-21 12:53:23 +0100

    Improve AES CBC example for Python.

--- Cryptography/AES-CBC/Python/aes_cbc.py
+++ Cryptography/AES-CBC/Python/aes_cbc.py
@@ -3,12 +3,12 @@ from Crypto.Util.Padding import pad, unpad
 
 iv = b'aaaaaaaaaaaaaaaa'
 key = b'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-text = 'This is a secret!'
+plaintext = 'This is a secret!'
 
 cipher = AES.new(key, AES.MODE_CBC, iv)
-ciphertext = cipher.encrypt(pad(text.encode("utf-8"), AES.block_size))
+ciphertext = cipher.encrypt(pad(plaintext.encode("utf-8"), AES.block_size))
 print(f'Encryption: {ciphertext.hex()}')
 
 cipher = AES.new(key, AES.MODE_CBC, iv)
-pt = unpad(cipher.decrypt(ciphertext), AES.block_size)
-print(f'Decryption: {pt.decode("utf-8")}')
+plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+print(f'Decryption: {plaintext.decode("utf-8")}')
